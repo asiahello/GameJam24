f91d889bb9dc2b3850f8b0a4366386d1045c53ac
Teodor Michalski <teodor.michalski@gmail.com>
2024-09-01 00:51:05 +0200

    Add traumas atlas, use for sprites

--- trauma/blue.go
+++ trauma/blue.go
@@ -30,11 +30,15 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"blue\"\n"
+  data: "default_animation: \"bubu\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/main/sprites.atlas\"\n"
+  "  texture: \"/main/traumas.atlas\"\n"
   "}\n"
   ""
+  scale {
+    x: 0.3
+    y: 0.3
+  }
 }
